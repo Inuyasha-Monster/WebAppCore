403b9334029ee2609ca8100ee08cb1d6ce5fe15e
djl <djl@DESKTOP-F5IDQLS>
2016-08-18 12:03:30 +0800

    添加项目文件。

--- src/WebAppCore/Program.cs
+++ src/WebAppCore/Program.cs
@@ -17,6 +17,30 @@ namespace WebAppCore
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+
+            host.Run();
+        }
+    }
+}
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebAppCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
             // 测试修改
             host.Run();
         }
